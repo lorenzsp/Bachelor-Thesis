e680f11df7c14890d4bc1c13e6c78b7aaba05fd9
lorenzsp <lorenzo.speri@gmail.com>
2018-07-01 22:36:53 +0200

    trajectories

--- gw/plot_f.m
+++ gw/plot_f.m
@@ -1,13 +1,13 @@
 function [] = plot_f(name, xax, yax, tick_size)
 %
 %title(name);
-xlabel(xax);
-ylabel(yax);
-ylh = get(gca,'ylabel');
+xlabel(xax,'interpreter','latex');
+ylabel(yax,'interpreter','latex');
+set(gca,'TickLabelInterpreter','latex');
+%ylh = get(gca,'ylabel');
 %gyl = get(ylh);                                                         % Object Information
 %ylp = get(ylh, 'Position');
 %set(ylh, 'Rotation',0, 'Position',ylp, 'VerticalAlignment','middle', 'HorizontalAlignment','right')
-set(gca,'TickLabelInterpreter','latex');
 set(gca,'FontSize',tick_size);
 %set(gca, 'FontName', 'Times');
 grid on;
