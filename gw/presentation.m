5881dde39443b54b233a129f67b475a8cd344254
lorenzsp <lorenzo.speri@gmail.com>
2018-07-11 23:14:49 +0200

    presentation animation

--- gw/presentation.m
+++ gw/presentation.m
@@ -1,3 +1,5 @@
+%% to create frames from gif
+% gifsicle --unoptimize ../times_pol.gif | convert - frame-%d.png
 %% wave polarizations animated
 %angle parameter of the ring
 theta = 0:0.03:2*pi;
@@ -61,51 +63,160 @@ end
 t=C;
 for n=1:10:length(C)
     
-    subplot(2,1,1),
+    subplot(2,2,1),
     plot(b4_p(ia(n),2),b4_p(ia(n),3),'.','MarkerSize',15);
     hold on;
     plot(-b4_p(ia(n),2),-b4_p(ia(n),3),'.','MarkerSize',15);
-    axis equal
-    xlim([-11 11]);
-    ylim([-11 11]);
+    %axis equal
+        pbaspect([1 1 1]);
+
+    xlim([-5 5]);
+    ylim([-5 5]);
+    plot_f('','$$x \; [M_{\odot}]$$','$$y \;[M_{\odot}]$$',16)
+    grid on;
     
-    subplot(2,1,2), 
+    subplot(2,2,2),
+    % plus polarization
+    h_plus = 200.*b4_h(ib(n),2);
+    h_times = 200.*b4_h(ib(n),3);
+    X = cos(theta) .* (1 + 0.5.*h_plus) + sin(theta).*(0.5.*h_times);
+    Y = sin(theta) .* (1 - 0.5.*h_plus) + cos(theta).*(0.5.*h_times);
+    plot(X,Y,'.');
+    xlim([-2 2]);
+    ylim([-2 2]);
+        grid on;
+            pbaspect([1 1 1]);
+
+
+    
+    
+    subplot(2,2,[3,4]), 
     hold on;
-    plot(b4_h(:,1),b4_h(:,2),'r');
-    plot(b4_h(:,1),b4_h(:,3),'b');
-    plot(t(n),b4_h(ib(n),2),'or','MarkerSize',4,'MarkerFaceColor','m') ;   
-    plot(t(n),b4_h(ib(n),3),'or','MarkerSize',4,'MarkerFaceColor','m');
+    plot(b4_h(:,1),b4_h(:,2).*115/(100.*Mparsec),'r');
+    plot(b4_h(:,1),b4_h(:,3).*115/(100.*Mparsec),'b');
+    plot(t(n),b4_h(ib(n),2).*115/(100.*Mparsec),'or','MarkerSize',4,'MarkerFaceColor','m') ;   
+    plot(t(n),b4_h(ib(n),3).*115/(100.*Mparsec),'or','MarkerSize',4,'MarkerFaceColor','m');
     xlim([0 400]);
+        grid on;
+plot_f('','$$t \,[M_{\odot}]$$','$$ h(t,r=100 Mpc) $$',16)
+    s= {['$$h_+$$'],...
+    ['$$h_{\times}$$']};
+    legend_f(s);
     
-    pause(0.01)
+    %pause(0.01)
+    if n==1
+        gif('bbh-b4.gif','DelayTime',0.2,'LoopCount',5,'frame',gcf);
+    else
+    
+    gif
+    end
     clf;
 
 end
 
-for n=ib(end):24:24849
+for n=ib(end):40:24849
     
-    subplot(2,1,1),
-    plot(0,0,'.','MarkerSize',15);
+    subplot(2,2,1),
+    plot(0,0,'k.','MarkerSize',15);
     hold on;
-    %plot(-b4_p(ia(n),2),-b4_p(ia(n),3),'.','MarkerSize',15);
-    axis equal
-    xlim([-11 11]);
-    ylim([-11 11]);
+    plot_f('','$$x \; [M_{\odot}]$$','$$y \;[M_{\odot}]$$',16)
+xlim([-5 5]);
+ylim([-5 5]);
+    pbaspect([1 1 1]);
+
+            grid on;
+
     
-    subplot(2,1,2), 
+    subplot(2,2,2),
+    h_plus = 200.*b4_h((n),2);
+    h_times = 200.*b4_h((n),3);
+    X = cos(theta) .* (1 + 0.5.*h_plus) + sin(theta).*(0.5.*h_times);
+    Y = sin(theta) .* (1 - 0.5.*h_plus) + cos(theta).*(0.5.*h_times);
+    plot(X,Y,'.');
+    xlim([-2 2]);
+    ylim([-2 2]);
+        pbaspect([1 1 1]);
+        grid on;
+    
+    subplot(2,2,[3,4]), 
     hold on;
-    plot(b4_h(:,1),b4_h(:,2),'r');
-    plot(b4_h(:,1),b4_h(:,3),'b');
-    plot(b4_h(n,1),b4_h(n,2),'or','MarkerSize',4,'MarkerFaceColor','m') ;   
-    plot(b4_h(n,1),b4_h(n,3),'or','MarkerSize',4,'MarkerFaceColor','m');
+    plot(b4_h(:,1),b4_h(:,2).*115/(100.*Mparsec),'r');
+    plot(b4_h(:,1),b4_h(:,3).*115/(100.*Mparsec),'b');
+    plot(b4_h(n,1),b4_h(n,2).*115/(100.*Mparsec),'or','MarkerSize',4,'MarkerFaceColor','m') ;   
+    plot(b4_h(n,1),b4_h(n,3).*115/(100.*Mparsec),'or','MarkerSize',4,'MarkerFaceColor','m');
     xlim([0 400]);
+            grid on;
+    plot_f('','$$t \,[M_{\odot}]$$','$$ h(t,r=100 Mpc) $$',16)
+    s= {['$$h_+$$'],...
+    ['$$h_{\times}$$']};
+    legend_f(s);
+    %115*/(100.*Mparsec)
     
-    pause(0.01)
+    %pause(0.01)
+    gif
     clf;
 
 end
 
-%%
+%% travelling GW
+%angle parameter of the ring
+theta = 0:0.01:2*pi;
+%frequency of the wave
+omega = 3;
+% t time
 
+n=1;
+ii=1;
+z=0.5:0.07:15;
+figure();
+for t=0.01:0.05:2.0944
+    % plus polarization
+    
 
+    plot3(0,2*cos(omega.*(t)),2*sin(omega.*(t)),'.','MarkerSize',20);
+    hold on;
+    plot3(0,-2*cos(omega.*(t)),-2*sin(omega.*(t)),'.','MarkerSize',20);
+    axis equal
+    CM = jet(250); % n+10 
+    %for z=0.5:0.1:4;
+    h_plus = -cos(2*omega.*(t-z ))./(z);
+    h_times = -sin(2*omega.*(t-z ))./(z);
+    %X = cos(theta) .* (1 + 0.5.*h_plus) + sin(theta).*(0.5.*h_times);
+    %Y = sin(theta) .* (1 - 0.5.*h_plus) + cos(theta).*(0.5.*h_times);
+    plot3(z,h_plus,0.*ones(size(z)));%,'.','color',CM(n,:));%z.*round(10*z)
+    area(z,h_plus);
+
+    plot3(z,0.*ones(size(z)),h_times); 
+    fill3(z([1 1:end 1]),0.*ones(length(z)+2), [0 h_times 0], 'r');
+    
+    grid on;
+    %set(gca,'Visible','off')
+    pbaspect([1 1 1]);
+    %set(gca,'zticklabel',[])
+    %set(gca,'xticklabel',[])
+    %set(gca,'yticklabel',[])
+    %plot_f('','z','h',20);
+    xlabel('z');
+    ylabel('h_+');
+    zlabel('h_\times');
+    %set(gca,'xtick',[])
+    %set(gca,'ytick',[])
+    %set(gca,'ztick',[])
+    xlim([0 6]);
+    zlim([-2 2]);
+    ylim([-2 2]);
+    view(37.5000,30)
+    %pause(0.00005)
+    n=n+1;
+    hold on;
+    if t==0.01
+        gif('gw_travelling.gif','DelayTime',0.2,'LoopCount',5,'frame',gcf);
+    else
+    
+    gif
+    end
+    
+    clf;
+    ii = ii + 0.2;
+end
 
